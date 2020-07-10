6a8f663ec807fc1c568ff25e054a4576dab7e50e
hafster18 <hmaxs@unite.edu>
2020-07-09 20:48:06 -0600

    recursivo text

--- recursividad.cpp
+++ recursividad.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 using namespace std;
+//recusividad: se llama a si misma
+//caso base: es el que me permite terminar la recursividad
+//caso general: es l aque hace la recursividad8 
 void saludar(int contador)
 {
 contador++;
 if (contador<5)
 {
-    
+    cout<<contador<<endl;
     saludar(contador);
 }
 else{
     cout<<"Hola"<<endl;
+    
 }
 
 }
