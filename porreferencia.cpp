c6228c90833151ee5e8a274106972a05a5ecb965
hafster18 <hmaxs@unite.edu>
2020-07-09 20:23:59 -0600

    tipos de parametros

--- porreferencia.cpp
+++ porreferencia.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 using namespace std;
 //referencia por la posicion
-void referencia (int numero1, int& numero2) //parametros por referencia cuando lleva un asperson
+//parametros por referencia, valor, opcional
+//int numero1 =5 es un parametro opcional
+void referencia (int& numero2,int numero1 =5) //parametros por referencia cuando lleva un asperson
 {
 cout<<"Numero 1:  "<<numero1<<endl;
 cout<<"Numero 2:  "<<numero2<<endl;
-numero1=1;
+//numero1=1;
 numero2=100;
 }
 int main ()
 {
-int numero1, numero2;
-cout<<"Ingrese el numero 1: ";
-cin>>numero1;
+int numero1=0, numero2;
+//cout<<"Ingrese el numero 1: ";
+//cin>>numero1;
 cout<<"Ingrese el numero 2: ";
 cin>>numero2;
-referencia(numero1,numero2);
+referencia(numero2);
 cout<<"Numero 1:  "<<numero1<<endl;
 cout<<"Numero 2:  "<<numero2<<endl;
 
